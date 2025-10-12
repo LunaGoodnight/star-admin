05d732ba196cf4649cecd20b2de5051e2e2a36a1
Luna <javaspringtea@gmail.com>
2025-10-12 23:54:15 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import Tiptap from "@/app/Tiptap";
+import {SimpleEditor} from "@/components/tiptap-templates/simple/simple-editor";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Tiptap />
-      </main>
-   </div>
-  );
+    return (
+        <div
+            className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <SimpleEditor/>
+            </main>
+        </div>
+    );
 }
