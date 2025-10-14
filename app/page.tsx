af5f02632d8a9c4c013cef7a7f3ee03075d8e1ce
Luna <javaspringtea@gmail.com>
2025-10-14 23:49:40 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import {SimpleEditor} from "@/components/tiptap-templates/simple/simple-editor";
 
 export default function Home() {
     return (
-        <div
-            className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="flex flex-col gap-20 p-6">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center">
                 <SimpleEditor/>
             </main>
         </div>
